cb165dc550406f475567b4fdf770ad9cd55c5ed1
Arayik <Arayik.Harutyunyan>
2021-10-25 05:13:54 +0400

    fix

--- playlist/src/main/java/com/demo/playlist/PlaylistApplication.java
+++ playlist/src/main/java/com/demo/playlist/PlaylistApplication.java
@@ -6,8 +6,8 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class PlaylistApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(PlaylistApplication.class, args);
-	}
+  public static void main(String[] args) {
+    SpringApplication.run(PlaylistApplication.class, args);
+  }
 
 }
